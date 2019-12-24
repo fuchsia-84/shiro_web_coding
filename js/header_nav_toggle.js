e4322175fed6a3ea4859a399c6bb684ca4543e52
fuchsia-84 <mikitech9084@gmail.com>
2019-12-24 15:10:36 +0900

    add header_contents class, nav links, nav  scripts

--- js/header_nav_toggle.js
+++ js/header_nav_toggle.js
@@ -11,5 +11,17 @@ $(function() {
     // Nav Toggle Button
     $('#nav_toggle').click(function(){
         $header.toggleClass('open');
+        // Global Nav
+        if ( $('#header').hasClass('open') ) {
+            $('#global_nav').slideDown();
+            $('#global_nav').css({display: block})
+        } else {
+            $('#global_nav').slideUp();
+            $('#global_nav').css({display: none})
+        }
+    });
+    // Global Nav Links
+    $('#global_nav li a').on('click', function(event){
+        $('#nav_toggle').trigger('click');
     });
 });
