533404d83debf96f1d87c7ae5d7e7354bba8c9f6
fuchsia-84 <mikitech9084@gmail.com>
2019-12-26 01:48:06 +0900

    adjust change header position.

--- js/header_nav_toggle.js
+++ js/header_nav_toggle.js
@@ -1,8 +1,9 @@
 $(function() {
     var $header = $('#header');
+    var change_header_position = $("#top").height() - $("#header").outerHeight(true);
     // Nav Fixed
     $(window).scroll(function() {
-        if ($(window).scrollTop() > 556) { //top - header_height
+        if ($(window).scrollTop() > change_header_position) { 
             $header.addClass('fixed');
         } else {
             $header.removeClass('fixed');
